561b2991a63f4a5d074441161d41e2a613d485cb
Areli Pérez <66649193+Areli-ITCG@users.noreply.github.com>
2022-03-03 16:04:18 -0600

    Update Principal.java
    modificación de la clase principal.

--- Agenda/Principal.java
+++ Agenda/Principal.java
@@ -2,6 +2,6 @@ public class Principal
 {
    public static void main (String [] args)
    {
-     JFrameMostrar elist = new JFrameMostrar();
+     JFrameIngreso2022 elist = new JFrameIngreso2022();
    }
 }
